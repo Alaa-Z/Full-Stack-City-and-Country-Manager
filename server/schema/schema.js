147bc909722f456d8db19e6bfc9e1d7ffff92252
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-12 18:02:09 +0200

    add RootQuery and import schema in app.js

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,14 +3,32 @@ const graphql = require('graphql');
 const {
     GraphQLObjectType,
     GraphQLInt,
-    GraphQLString
+    GraphQLString,
+    GraphQLSchema
 } = graphql;
 
 const CityType = new GraphQLObjectType({
     name: 'City',
     fields: ()=>({
         id: {type: GraphQLInt},
-        name:{type:GraphQLString},
-        description:{type:GraphQLString}
+        name:{type: GraphQLString},
+        description:{type: GraphQLString}
     })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        city: {
+            type: CityType,
+            args:{ id: {type: GraphQLInt}},
+            resolve(parent, args){
+                // code to get data from db
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
