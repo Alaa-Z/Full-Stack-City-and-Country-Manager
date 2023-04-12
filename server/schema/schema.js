a0f7662f049612638f291fe9cee84e8be49adaeb
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-12 18:47:07 +0200

    add CountryType and new query for country

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,7 +11,14 @@ const {
 // Data for cities
 var cities = [
     {name:'Lund', id: "1", description:"description 1"},
-    {name:'Växjö', id: "2", description:"description 2"}
+    {name:'Växjö', id: "2", description:"description 2"},
+    {name:'New York', id: "3", description:"description 3"}
+]
+
+var countries = [
+    {name: 'Sewden', id: "1", population: '10.42000000' },
+    {name: 'USA', id: "2", population: '331.9000000' }
+
 ]
 const CityType = new GraphQLObjectType({
     name: 'City',
@@ -19,6 +26,20 @@ const CityType = new GraphQLObjectType({
         id: {type: GraphQLID},
         name:{type: GraphQLString},
         description:{type: GraphQLString}
+    })
+})
+
+const CountryType = new GraphQLObjectType({
+    name: 'Country',
+    fields: ()=>({
+        id: {type: GraphQLID},
+        name:{type: GraphQLString},
+        population: {
+            type: GraphQLString,
+            resolve(parent) {
+                return parent.population.toLocaleString();
+            }
+        }
     })
 })
 
@@ -31,6 +52,14 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args){
                 // code to get data from db
                 return _.find(cities, {id: args.id})
+            }
+        },
+        country: {
+            type: CountryType,
+            args:{ id: {type: GraphQLID}},
+            resolve(parent, args){
+                // code to get data from db
+                return _.find(countries, {id: args.id})
             }
         }
     }
