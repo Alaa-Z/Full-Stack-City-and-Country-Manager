316b60063f68e0787db133cb9730514431b332cb
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-13 11:12:25 +0200

    add citylist componsnt

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
+import react from "react";
+// import components
+import CityList from "./components/CityList";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1> List </h1>
+      <CityList/>
     </div>
   );
 }
---
+++ client/src/components/CityList.js
@@ -0,0 +1,16 @@
+
+function CityList() {
+    return (
+      <div >
+        <ul id="city-list">
+            <li> city 1</li>
+            <li> city 2</li>
+            <li> city 3</li>
+
+        </ul>
+      </div>
+    );
+  }
+  
+  export default CityList;
+  
