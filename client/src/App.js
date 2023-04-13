d0058c56b1b37c4c16eb4659faf8ed992c33aea6
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-13 13:31:31 +0200

    install apoloClient and setup apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
-import react from "react";
-// import components
+import React from "react";
 import CityList from "./components/CityList";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+// setup appolo client
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1> List </h1>
-      <CityList/>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h1>List</h1>
+        <CityList />
+      </div>
+    </ApolloProvider>
   );
 }
 
