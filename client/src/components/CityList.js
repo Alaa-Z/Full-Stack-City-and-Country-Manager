eb53d92cff331f3052bd1a46bf55ed9475764845
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-13 14:09:53 +0200

    display cities with map()

--- client/src/components/CityList.js
+++ client/src/components/CityList.js
@@ -12,16 +12,22 @@ const getCitiesQuery = gql`
 `
 
 function CityList() {
+
     const { loading, error, data } = useQuery(getCitiesQuery);
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
-    console.log(data);
+    // console.log(data);
+    const displayCity = (city) => {
+        return (
+            <>
+                <li key={city.id}> {city.name}</li> 
+            </>
+        );
+    }
     return (
       <div >
-        <ul id="city-list">
-            <li> city 1</li>
-            <li> city 2</li>
-            <li> city 3</li>
+        <ul id="city-list">  
+            {data.cities.map(city =>displayCity(city))}          
         </ul>
       </div>
     );
